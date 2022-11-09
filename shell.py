5980ae345972d53ef4442fd870abf6349f10dad2
Allen CArl Delas Alas <114001078+KuyaAC@users.noreply.github.com>
2022-11-09 10:33:06 +0800

    fixing bugs

--- shell.py
+++ shell.py
@@ -5,5 +5,5 @@ while True:
     result, error = basic.run('<stdin>', text)
 
 
-if error: print(error.as_string())
-else: print(result)
+    if error: print(error.as_string())
+    else: print(result)
