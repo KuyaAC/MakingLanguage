213d2e806a1a5533bcdad9c9ef407678ccf2dd71
Allen CArl Delas Alas <114001078+KuyaAC@users.noreply.github.com>
2022-11-09 10:32:18 +0800

    fixing shell problems

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import basic
 
 while True:
     text = input('Maglagay >')
-    result, error = basic.run(text)
+    result, error = basic.run('<stdin>', text)
 
 
 if error: print(error.as_string())
