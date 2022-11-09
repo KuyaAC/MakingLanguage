f47d617487e0a1b95fab310dcf7e051091468e29
KuyaAC <aciesnet02@gmail.com>
2022-11-09 10:14:16 +0800

    create token and lexer

--- basic.py
+++ basic.py
@@ -1,4 +1,28 @@
 ##############
+# CONSTANT
+##############
+
+DIGITS = '0123456789'
+
+##############
+# ERROR
+##############
+
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        return result
+
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__('Illegal Character', details)
+
+
+##############
 # TOKENS
 ##############
 
@@ -12,7 +36,7 @@ TT_LPAREN   = 'LPAREN'
 TT_RPAREN   = 'RPAREN'
 
 class Token:
-    def __init__(self, type_, value):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
@@ -20,3 +44,81 @@ class Token:
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
+
+##############
+# LEXER
+##############
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens =[]
+
+        while self.current_char != None:
+            if self.current_char in '\t':
+                self.advance(self.make_num())
+            elif self.current_char in DIGITS:
+                tokens.append
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            else:
+                #return an  error
+                char = self.current_char
+                self.advance
+                return [], IllegalCharError("'" + char + "'")
+
+        return tokens, None
+    #determine digit
+    def make_number(self):
+        numb_str = ''
+        dot_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.' :
+                if dot_count == 1: break
+                dot_count += 1
+                numb_str += '.'
+            else:
+                numb_str += self.current_char
+            self.advance()
+                
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
+##############
+# RUN
+##############
+
+def run(text):
+    lexer = Lexer(text)
+    tokens, error = lexer.make_tokens()
+
+    return tokens, error
+
+
+
